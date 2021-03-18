b1aa3277a534f3ee3013e3bc5040a648fa3ca3bf
lazmond3 <moikilo00+xcode@gmail.com>
2021-03-18 23:29:18 +0900

    DELETE

--- twitter_cli/cli.py
+++ twitter_cli/cli.py
@@ -1,4 +1,3 @@
-import twitter
 import os
 
 CONSUMER_KEY=os.getenv("CONSUMER_KEY")
@@ -13,16 +12,3 @@ if not all([
     TOKEN_SECRET ]):
     print("Specify all env: CONSUMER_KEY, CONSUMER_SECRET, TOKEN, TOKEN_SECRET")
     exit(0)
-
-# 取得したキーとアクセストークンを設定する
-auth = twitter.OAuth(consumer_key=CONSUMER_KEY,
-                     consumer_secret=CONSUMER_SECRET,
-                     token=TOKEN,
-                     token_secret=TOKEN_SECRET)
-
-cli = twitter.Twitter(auth=auth)
-
-# twitterへメッセージを投稿する 
-# t.statuses.update(status="pythonからtwitterへの投稿テストです！")
-def update(text: str)-> None: 
-    cli.statuses.update(status=text)
