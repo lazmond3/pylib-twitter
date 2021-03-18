986a65413b4f7530ba8819347b6f7db1ac0c292c
lazmond3 <moikilo00+xcode@gmail.com>
2021-03-18 23:53:48 +0900

    ADD application level oauth

--- twitter_cli/cli.py
+++ twitter_cli/cli.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from .base64_util import base64_encode_str
 
 CONSUMER_KEY=os.getenv("CONSUMER_KEY")
@@ -20,5 +21,28 @@ def make_basic(consumer_key: str, consumer_secret: str) -> str:
     converted = base64_encode_str(concat)
     return converted
 
+def get_auth(url: str, basic: str):
+    headers = {"Authorization": f"Basic {basic}"}
+    payload = {"grant_type": "client_credentials"}
+    r = requests.post(url, headers=headers, params=payload)
+
+    print(r)
+    print("-------")
+    print(r.text)
+    with open("dump.json", "w") as f:
+        f.write(r.text)
+
 if __name__ == '__main__':
     print(make_basic("hello", "world"))
+    if not all([
+        CONSUMER_KEY, CONSUMER_SECRET
+    ]):
+        print("specify consumer key/secret")
+        exit(1)
+
+    basic = make_basic(CONSUMER_KEY, CONSUMER_SECRET)
+    print(basic)
+    url = "https://api.twitter.com/oauth2/token"
+    get_auth(url, basic)
+    # basic
+    
